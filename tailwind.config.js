c6ce1a7d80828826324afdb4c377766fce8b033d
agtaylor010 <andrew.taylor2@education.wa.edu.au>
2023-06-26 16:45:41 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3F2B56'
+      }
+    },
   },
   plugins: [],
 }
