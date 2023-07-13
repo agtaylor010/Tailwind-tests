871b780fd17baf560d40ecfced757de9e3a2843b
agtaylor010 <andrew.taylor2@education.wa.edu.au>
2023-07-13 13:17:28 +0800

    Font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
+    fontFamily: {
+      'display': ['puffin-display'],
+      'body': ['"puffin-display-soft"'],
+    },
     extend: {
       colors: {
         primary: '#3F2B56',
