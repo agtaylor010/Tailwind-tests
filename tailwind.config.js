8fa83a2ebef72a95ba4d6e3eea78a037f7a1441c
agtaylor010 <andrew.taylor2@education.wa.edu.au>
2023-07-14 10:43:16 +0800

    Added a max-screen dec for config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
+    screens: {
+      'sm': {'max': '639px'},
+
+    },
     fontFamily: {
       'display': ['puffin-display'],
       'body': ['"puffin-display-soft"'],
     },
     extend: {
-      screens: { 'sm': { 'max': '640px' } },
       colors: {
         primary: '#3F2B56',
         secondary: '#9e0f6d'
